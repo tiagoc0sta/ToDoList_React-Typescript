c6f317b5b458c934ecbe2f6ff2e4c062df851fd8
Tiago Costa <tiagodpv@gmail.com>
2022-08-31 20:45:01 -0400

    add positioning central

--- praticando-os-conceitos/src/components/Tasks/index.tsx
+++ praticando-os-conceitos/src/components/Tasks/index.tsx
@@ -1,53 +1,54 @@
-import { TbClipboard, TbClipboardText } from 'react-icons/tb';
-import { ITask } from '../../App';
-import { Task } from '../Task';
-import styles from './tasks.module.css';
+import { TbClipboardText } from "react-icons/tb";
+import { ITask } from "../../App";
+import { Task } from "../Task";
+import styles from "./tasks.module.css";
 
 interface Props {
   tasks: ITask[];
-  onDelete: (taskId: string) => void;
   onComplete: (taskId: string) => void;
+  onDelete: (taskId: string) => void;
 }
 
-export function Tasks({tasks, onDelete, onComplete }: Props){
-  const taskQuantity = tasks.length;
-  const completedTasks = tasks.filter((task)=> task.isCompleted).length;
+export function Tasks({ tasks, onComplete, onDelete }: Props) {
+  const tasksQuantity = tasks.length;
+  const completedTasks = tasks.filter((task) => task.isCompleted).length;
 
-  return(
+  return (
     <section className={styles.tasks}>
       <header className={styles.header}>
         <div>
-          <p>Created Tasks</p>
-          <span>{taskQuantity}</span>
+          <p>Created tasks</p>
+          <span>{tasksQuantity}</span>
+        </div>
 
-          <div>
-            <p className={styles.textPurple}>Finished</p>
-            <span> {completedTasks} of {taskQuantity}</span>
-          </div>
+        <div>
+          <p className={styles.textPurple}>Finished Tasks</p>
+          <span>
+            {completedTasks} de {tasksQuantity}
+          </span>
         </div>
       </header>
-        <div className={styles.list}>
-          {tasks.map((task) =>(
-            <Task 
-              key={task.id} 
-              task={task} 
-              onDelete={onDelete}
-              onComplete={onComplete}
-              />
-          ))}
 
-          {tasks.length <= 0 && (
-            <section className={styles.empty}>
-              <TbClipboardText size={50}/>
-              <div>
-                <p>You do not have registered tasks yet</p>
-                <span>Create tasks and organize yous ToDo List</span>
-              </div>
-            </section>
-          )}  
+      <div className={styles.list}>
+        {tasks.map((task) => (
+          <Task
+            key={task.id}
+            task={task}
+            onComplete={onComplete}
+            onDelete={onDelete}
+          />
+        ))}
 
-        </div>
-
+        {tasks.length <= 0 && (
+          <section className={styles.empty}>
+            <TbClipboardText size={50} />
+            <div>
+              <p>You do not have created a task yet</p>
+              <span>Create tasks to organize toDo List</span>
+            </div>
+          </section>
+        )}
+      </div>
     </section>
-  )
+  );
 }
